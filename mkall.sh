24b1de29c0fb2cfe20fd75b09e54ea7a1cee2244
Jim Gettys <jg@freedesktop.org>
2011-07-08 18:20:42 -0400

    Clean up the html in the script; not tableified yet.

--- mkall.sh
+++ mkall.sh
@@ -11,17 +11,10 @@ echo '<!DOCTYPE HTML>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head> 
  
-<body> 
-<nav>
-<a href="/uberwrt">Home</a> |
-<a href="/bgi-bin/redir.sh">Administer</a> |
-<a href="about.html">About UberWrt</a> |
-<a href="credits.html">Credits</a> |
-<a href="support.html">Help</a>
-</nav> 
 <div>'
-echo '<h2><a href="http://www.bufferbloat.net/projects/bloat">Bufferbloat Project</a></h2>
-<p align="center"><a href="http://gettys.wordpress.com">Jim&nbsp;Gettys</a>
+echo '<h1>CeroWrt Credits</h1>
+<h2><a href="http://www.bufferbloat.net/projects/bloat">Bufferbloat Project</a></h2>
+<p align="center"><a href="http://en.wikipedia.org/wiki/Jim_Gettys">Jim&nbsp;Gettys</a>
 <a href="http://www.taht.net">Dave&nbsp;Täht</a></p>'
 
 ./mkcredits bloat.list 
@@ -61,5 +54,18 @@ for their support and interest in this work</a>. </p>'
 echo '<p> There are no doubt thousands of other contributors uncredited, (notably everyone that developed Unix and Linux!) But if you have been involved in fixing bufferbloat, and we missed you on this list, let us know. </p>'
 echo '<p> This release of CeroWrt is dedicated to the memory of <a href="http://tools.ietf.org/html/rfc2468">Jonathon B. Postel</a>, who with his persistence, passion, and equanimity, helped get the Internet this far. '
 echo '</div>'
-echo '</body></html>'
+echo '<div class="menu">
+<nav>
+<ul role="navigation">
+<li><a href="/uberwrt/index.html">Home</a>
+<li><a href="/bgi-bin/redir.sh">Administer</a>    
+<li><a href="http://www.bufferbloat.net/">About Bufferbloat</a>
+<li><a href="about.html">About UberWrt</a>
+<li><a href="credits.html">Credits</a>
+<li><a href="support.html">Help</a>
+</ul>
+</nav> 
+</div>
+</body>
+</html>'
 ) > credits.html
