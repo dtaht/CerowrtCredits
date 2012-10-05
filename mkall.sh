3f4ff24991c58198725a434ef549a42d598c2487
Dave Täht <dave.taht@bufferbloat.net>
2012-10-05 10:50:02 -0700

    Still have to sort through the corp donors list

--- mkall.sh
+++ mkall.sh
@@ -13,11 +13,11 @@ echo '<!DOCTYPE HTML>
 </head>
 <body>'
 echo '<div class="center"><h1>CeroWrt Credits</h1>'
-echo '<p><i>This release of CeroWrt is dedicated to the memory of <a href="http://www.spcecdt.com/">John H Dubois</a>, who spread email and netnews through Santa Cruz, Ca, <a href="http://armory.com">housed many a geek</a>, and brought <a href="http://www.armory.com/~spcecdt/cooking/recipes/chocolateIceCream.html">liquid nitrogen ice cream</a> to Burning Man...</i></p><p><i> And to <a href="http://digital-rag.com">Richard Pitt</a>, who helped bring the Internet to western Canada, wrote <a href="http://digital-rag.com/article.php/Buffer-Bloat-Packet-Loss">the Digital Rag</a>, and put <a href="http://www.hancockwildlife.org/article.php/RichardPittFund">webcams in eagles nests</a>. </i></p>'
+echo '<p><i>This release of CeroWrt is dedicated to the memory of <a href="http://www.spcecdt.com/">John H Dubois</a>, who spread email and netnews through Santa Cruz, Ca, <a href="http://armory.com">housed many a geek</a>, and brought <a href="http://www.armory.com/~spcecdt/cooking/recipes/chocolateIceCream.html">liquid nitrogen ice cream</a> to Burning Man... And to <a href="http://digital-rag.com">Richard Pitt</a>, who helped bring the Internet to western Canada, wrote <a href="http://digital-rag.com/article.php/Buffer-Bloat-Packet-Loss">the Digital Rag</a>, and put <a href="http://www.hancockwildlife.org/article.php/RichardPittFund">webcams in eagles nests</a>. </i></p>'
 
 # Do openwrt up right
 echo '</div>'
-echo '<div class="center"><i>We are deeply indebted to the following members of the OpenWrt project for their help on <abbr title="Internet Relay Chat">IRC</abbr> and email and for helping create OpenWrt itself.</i>'
+echo '<div class="center"><b>We are deeply grateful to the following members of the OpenWrt project for their help on <abbr title="Internet Relay Chat">IRC</abbr> and email and for helping create OpenWrt itself.</b>'
 echo '<h2><a href="http://www.openwrt.org">OpenWrt</a></h2>'
 echo '<table><tr><td>'
 C=`cat openwrt.list | wc -l`
@@ -30,8 +30,8 @@ J=$(($J + 1))
 done
 echo '</td></tr></table>'
 echo '</div>'
-echo '<div class="center">The CeroWrt effort would have died without the timely intervention and financial support of the following</div>'
-echo '<div class="center"><h2>Donors and Subscribers</h2></div>'
+echo '<div class="center"><br /><b>The CeroWrt effort would <a href="http://www.teklibre.com/cerowrt/subscribe.html">die without financial support</a>. We are indebted to the following</b>'
+echo '<h2><a href="http://www.teklibre.com/cerowrt/subscribe.html">CeroWrt Donors and Subscribers</a></h2>'
 echo '<table><tr><td class="narrow">'
 C=`cat subscribers.list | wc -l`
 J=0
@@ -52,11 +52,12 @@ done
 echo '</td></tr></table>'
 echo '</div>'
 
-echo '<center><h2><a href="http://www.bufferbloat.net/projects/bloat">Bufferbloat Project</a></h2>
+echo '<div class="center"><br /><b>CeroWrt is a sub-project of the overall de-bloating efforts of the members of</b><h2><a href="http://www.bufferbloat.net/projects/bloat">The Bufferbloat Project</a></h2>
 <p><a href="http://gettys.wordpress.com/">Jim&nbsp;Gettys</a>
 <a href="http://www.taht.net">Dave&nbsp;Täht</a></p></center>'
 
 ./mkcredits bloat.list
+echo '</div>'
 echo '<div class="center"><h2>Special Thanks</h2>'
 
 echo '<table><tr><td class="narrow">'
@@ -76,15 +77,17 @@ echo '<p>We thank the
 <a href="http://isc.org">Internet Systems Consortium</a>,
 <a href="http://www.alcatel-lucent.com">Alcatel-Lucent Bell Labs</a>,
 <a href="http://www.candelatech.com">Candela</a>,
+<a href="http://www.cisco.com">Cisco</a>,
 <a href="http://www.comcast.com">Comcast</a>,
+<a href="http://www.gatech.edu">Georgia Institute of Technology</a>, and
 <a href="http://www.google.com">Google</a>,
 <a href="http://www.intermapper.com">InterMapper</a>,
 <a href="http://www.teklibre.com">Teklibre</a>,
 <a href="http://www.mit.edu">Massachusetts Institute of Technology</a>,
 <a href="http://www.stanford.edu">Stanford University</a>,
-<a href="http://www.gatech.edu">Georgia Tech</a>, and
-<a href="http://www.lincs.fr">the Lincs lab</a>
-for their support and interest in the bufferbloat work.</p>'
+<a href="http://www.lincs.fr">the Lincs lab</a>, and
+<a href="http://www.vyatta.com">Vyatta</a>
+for their support of and interest in the bufferbloat work.</p>'
 
 echo '<p> There are no doubt thousands of other contributors uncredited, (notably nearly everyone that developed the Internet, Unix and Linux!) But if you have been involved in fixing bufferbloat, and we missed you on this list, let us know. </p>'
 echo '<!-- The author of the scripts that generated this file freely confesses to be terrible at html and html5 -->'
