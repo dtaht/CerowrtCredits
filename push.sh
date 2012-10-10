448c6d1dd3aaca9c0c05bb707a388ec1983d74eb
Dave Täht <dave.taht@bufferbloat.net>
2012-10-09 21:01:25 -0700

    More credits fixes

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 SERVER=cero1@huchra.bufferbloat.net
+SERVER2=d@huchra.bufferbloat.net
 SDIR=src/ceropackages/utils/cerowrt-chrome/files/etc/cerowrt
 ./mkall.sh
 scp credits.html ${SERVER}:${SDIR}
 ssh $SERVER "cd ${SDIR}; git pull; git commit -m 'update_credits' credits.html; git push;"
+ssh $SERVER2 "cd ${SDIR}; git pull"
